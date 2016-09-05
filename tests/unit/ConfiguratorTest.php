7858656ee6fdae91946f819b74b590ed60f49c2c
Tom Oram <tom@x2k.co.uk>
2016-09-05 19:59:14 +0100

    Throw an exception when file pattern matching fails

---
+++ tests/unit/ConfiguratorTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tests\unit\TomPHP\ConfigServiceProvider;
+
+use PHPUnit_Framework_TestCase;
+use tests\support\TestFileCreator;
+use TomPHP\ConfigServiceProvider\Configurator;
+
+final class ConfiguratorTest extends PHPUnit_Framework_TestCase
+{
+    use TestFileCreator;
+
+    public function testItThrowsAnExceptionWhenNoFilesAreNotFound()
+    {
+        $this->setExpectedException('TomPHP\ConfigServiceProvider\Exception\NoMatchingFilesException');
+
+        Configurator::apply()->configFromFiles($this->getTestPath('config.php'));
+    }
+}
