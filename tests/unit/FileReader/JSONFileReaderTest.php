4b8c94e32bea56299393e58417f670808aaa2e02
Andreas Möller <am@localheinz.com>
2016-09-17 14:48:10 +0200

    Fix: Run 'composer cs:fix'

--- tests/unit/FileReader/JSONFileReaderTest.php
+++ tests/unit/FileReader/JSONFileReaderTest.php
@@ -4,10 +4,10 @@ namespace tests\unit\TomPHP\ContainerConfigurator\FileReader;
 
 use InvalidArgumentException;
 use PHPUnit_Framework_TestCase;
+use tests\support\TestFileCreator;
 use TomPHP\ContainerConfigurator\Exception\InvalidConfigException;
 use TomPHP\ContainerConfigurator\FileReader\FileReader;
 use TomPHP\ContainerConfigurator\FileReader\JSONFileReader;
-use tests\support\TestFileCreator;
 
 final class JSONFileReaderTest extends PHPUnit_Framework_TestCase
 {
